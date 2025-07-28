3f83f17757f7a93d4c6507ef4c512b6ea8465000
Ofallbrains <ofallbrains@gmail.com>
2025-07-28 15:27:57 +0200

    Custom Componet in React

--- exercise-3/src/App.tsx
+++ exercise-3/src/App.tsx
@@ -1,9 +1,22 @@
 import Button from "./Button";
+import NavLists from "./NavLists";
 
 
 export default function App() {
+const navItems = ['Home', "Exercise 1", "Exercise 2", "Exercise 3", "Exercise 4", "Exercise 5", "Exercise 6", "Exercise 7", "Exercise 8", "Exercise 9"]
+
+  function handleClick(text: string) {
+   alert(`You clicked ${text}`)
+ }
   return (
-    <Button/>
+
+    <div>
+      <NavLists items={navItems}/>
+      <Button text="Button 1" onClick={() =>handleClick('Button 1')}/>
+      <Button text="Button 2" onClick={() =>handleClick('Button 2')}/>
+      <Button text="Button 3" onClick={() =>handleClick('Button 3')}/>
+
+    </div>
   )
 }
 
