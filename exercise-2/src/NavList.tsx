97b5c0b2add63e5ae515a5045650a01562923307
Ofallbrains <ofallbrains@gmail.com>
2025-07-28 14:46:06 +0200

    Capturing User Clicks

--- exercise-2/src/NavList.tsx
+++ exercise-2/src/NavList.tsx
@@ -7,7 +7,9 @@ export default function NavList({ items }: NavProps) {
         <>
             <ul className="flex ">
                 {items.map((item, index) => (
-                    <li key={index}>
+                    <li
+                        key={index}
+                        className="text-blue-500 underline">
                         {item}
                         {index < items.length - 1 && <span className="mx-2">|</span>}
                     </li>
